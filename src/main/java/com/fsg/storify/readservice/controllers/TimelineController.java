8292f88ca12a69aa8684b1e7e76904464f3dcd51
Alex O'Donovan <aodonovan@fexco.com>
2013-05-09 12:14:27 +0100

    add jsonp endpoint for editor functionailty (jsonp responses wrapped differenly for timelinejs and extjs)

--- src/main/java/com/fsg/storify/readservice/controllers/TimelineController.java
+++ src/main/java/com/fsg/storify/readservice/controllers/TimelineController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fsg.storify.readservice.timeline.Family;
@@ -16,7 +17,7 @@ import com.fsg.storify.readservice.timeline.Timeline;
 public class TimelineController {
 	
 	@RequestMapping(value="/{searchString}/data.jsonp", method = RequestMethod.GET, produces="plain/text")
-	public @ResponseBody String getData(@PathVariable String searchString){		
+	public @ResponseBody String getDataJsonp(@PathVariable String searchString){		
 		List<Family> resultList = Family.findFamilysByNameLike(searchString).getResultList();
 		Family family = resultList.get(0);
 		Timeline timeline = Timeline.findTimelinesByFamily(family).getSingleResult();	
@@ -25,5 +26,29 @@ public class TimelineController {
 									.append(timeline.getData());		
 		
 		return jsonP.toString();		
+	} 
+	
+	@RequestMapping(value="/{searchString}/data.jsonpp", method = RequestMethod.GET, produces="text/javascript")
+	public @ResponseBody String getDataForEdit(@PathVariable String searchString, @RequestParam(value="callback") String callback){
+		List<Family> resultList = Family.findFamilysByNameLike(searchString).getResultList();
+		Family family = resultList.get(0);
+		Timeline timeline = Timeline.findTimelinesByFamily(family).getSingleResult();	
+		
+		StringBuilder jsonP = new StringBuilder(callback+"(")
+									.append(timeline.getData())
+									.append(");");		
+		
+		return jsonP.toString();		
+		
+	}
+	
+	
+	
+	@RequestMapping(value="/{searchString}/data.json", method = RequestMethod.GET)
+	public @ResponseBody String getDataJson(@PathVariable String searchString){		
+		List<Family> resultList = Family.findFamilysByNameLike(searchString).getResultList();
+		Family family = resultList.get(0);
+		Timeline timeline = Timeline.findTimelinesByFamily(family).getSingleResult();						
+		return timeline.getData();		
 	} 
 }
